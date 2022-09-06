394ac955a19184a0f4ef04394b5c9a989dfdbaf7
issei <issei.hiramatsu@gmail.com>
2022-09-06 12:21:20 +0900

    null safethyを付与　！はNullに絶対にならない　？はNullになる可能性があるものにし可能性があるものにしようすっるあるものに使用する

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -29,7 +29,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
   void didChangeDependencies() {
     if (!_loadedInitData) {
       final routeArgs =
-          ModalRoute.of(context).settings.arguments as Map<String, String>;
+          ModalRoute.of(context)!.settings.arguments as Map<String, String>;
       categoryTitle = routeArgs['title'];
       final categoryId = routeArgs['id'];
       displayedMeals = widget.availableMeals.where((meal) {
